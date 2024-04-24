9173527810207958f6889720b0c0dcdea01c4fde
Absalom <mrclabsalom@gmail.com>
2024-04-24 18:41:25 +0300

    setup complete

--- Server/myproject/myproject/settings.py
+++ Server/myproject/myproject/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 # Application definition
 
@@ -38,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'ECommerceapp',
+    'rest_framework',
+    'ecommerce',
 ]
 
 MIDDLEWARE = [
